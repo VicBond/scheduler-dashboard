043a04109b5d51f0e1f8bea2276a067ccf984107
Victor Bondarenko <nirasimha@gmail.com>
2021-12-16 14:38:18 -0500

    connecting WebSockets

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import Loading from "components/Loading";
 import Panel from "components/Panel";
 import classnames from "classnames";
 import axios from "axios";
+import { setInterview } from "helpers/reducers";
 import {
   getTotalInterviews,
   getLeastPopularTimeSlot,
@@ -64,6 +65,17 @@ class Dashboard extends Component {
       });
     });
 
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
     if (focused) {
       this.setState({ focused });
     }
@@ -73,6 +85,10 @@ class Dashboard extends Component {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
   }
 
   selectPanel(id) {
